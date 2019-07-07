fba41b06c2e8946c7743ff5e429ac283e0c0a6c7
Jean-Marc Désiront <admin@MacBook-Pro-de-Jean-Marc.local>
2019-07-07 17:30:38 +0200

    Updates

---
+++ precache-manifest.0ae765ea0643d59f7f3fb1d1f975048d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50dfa6bd2d2258b49dae0cb3a23eb1b3",
+    "url": "/my-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "326b4fcd9bc4b1765075",
+    "url": "/my-monsters-rolodex/static/css/main.5e716036.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/my-monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "326b4fcd9bc4b1765075",
+    "url": "/my-monsters-rolodex/static/js/main.98e71cce.chunk.js"
+  },
+  {
+    "revision": "e815729af9d0faa5cdb8",
+    "url": "/my-monsters-rolodex/static/js/runtime~main.20245d6c.js"
+  }
+]);
