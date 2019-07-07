f6509db15891ab8555b1de43d736bbfd5a09d263
Jean-Marc Désiront <admin@MacBook-Pro-de-Jean-Marc.local>
2019-07-07 16:51:38 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+      ],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ monsters: users}))
+  }
+
+  handleChange = e => {
+     this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
+    return <div className='App'>
+        <h1> Monsters Rolodex </h1>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+          />
+      <CardList monsters={filteredMonsters} />
+      <h2>Do you want to learn how to create and deploy this website made in React and Javascript for yourself ?</h2>
+      <h1>Let's Check<a href='https://tinyurl.com/yynax3jy' target='_blank' rel='noopener noreferrer'> It </a>Out;)!</h1>
     </div>
-  );
+  }
 }
+
+
+
 
 export default App;
